275120037ce832ca0f5d7c39ae28a49447263ecb
Caroline Liu <carolinexwliu89@berkeley.edu>
2019-03-12 21:08:43 -0700

    updated testing code file w/o Raspberry Pi code b/c that's taking ages to download

--- id_input.py
+++ id_input.py
@@ -1,10 +1,13 @@
-id_text_opened = open("id_tags.txt")
-lst_of_authenticated_ids = [id for id in id_text_opened]
+#read file and allow appending (or do we want editing)
+id_text_opened = open("id_tags_tester.txt", "a")
+
+#lst of ids (ints)
+#lst_of_authenticated_ids = [id for id in id_text_opened]
 
 while True:
 	input_id = input()
 	#serial.strip() # basically converts int to string
-	if input_id in lst_of_authenticated_ids:
+	if input_id in id_text_opened:
 		print(input_id + " is verified")
 		door.open() #door is pseudocode
 		""" 
@@ -15,8 +18,15 @@ while True:
 		"""
 	else: #register number
 		print("Authentication required: ")
-		allowed = input() #from remote?? Or someone who is verified
-		if allowed:
+		allowed = input() #from remote?? Or someone who is verified by typing yes into monitor
+		#could also press button for true/false signal
+		if allowed is "yes": 
 			#add to database or write into text file
+			#currently we want to write directly into text file and SAVE it (and
+			#autopush or we could manually push)
+			id_text_opened.append(input_id)
+
 		else:
 			print(input_id + " is not a registered user of the Berkeley Disability Lab")
+#we are now all done working w/ the file
+id_text_opened.close()
